60456fe7cb9064ddcf6652ac672009d4152f2dd8
Kum Jude Tem <judekum14@gmail.com>
2020-08-25 10:39:50 +0100

    Added the toggler icon to the navbar

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  $nav = $(".nav");
+  $toggleCollapse = $(".toggle-collapse");
+
+  // click event on toggle menu
+  $toggleCollapse.click(() => {
+    $nav.toggleClass("collapse");
+  });
+});
