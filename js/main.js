8998b66e29ec48bfdc2358bbd046b4150d83a7ad
Kum Jude Tem <judekum14@gmail.com>
2020-08-26 13:00:03 +0100

    Added carousel and styling

--- js/main.js
+++ js/main.js
@@ -6,4 +6,14 @@ $(document).ready(() => {
   $toggleCollapse.click(() => {
     $nav.toggleClass("collapse");
   });
+
+  // owl-carousel
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText:[$('.owl-nav .owl-prev'), $('.owl-nav .owl-next')]
+  });
 });
